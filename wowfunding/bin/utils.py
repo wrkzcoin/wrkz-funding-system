518f739eb467ed8208a9757ccb18984cb8aa7e77
CamTheGeek <cam@camthegeek.net>
2018-08-30 20:33:06 -0400

    change stuff->value

--- wowfunding/bin/utils.py
+++ wowfunding/bin/utils.py
@@ -80,8 +80,8 @@ def price_tradeogre_wow_btc():
     headers = {'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'}
     try:
         r = requests.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-aeon', headers=headers)
-        stuff = r.json()['result'][0]['High']
-        return stuff
+        value = r.json()['result'][0]['High']
+        return value
     except:
         return
 
